298a7e393826cc3668854dbdf68571c38388cc77
Juan Pablo Rios Escobar <jprioses@outlook.com>
2023-08-22 11:22:26 -0500

    Done models, making user routes, auth middleware and controllers

--- services/jwt.js
+++ services/jwt.js
@@ -1,27 +1,25 @@
-const jwt = require('jwt-simple');
-const moment = require('moment');
+const jwt = require("jwt-simple");
+const moment = require("moment");
 
 //Secret key to generate token, to code and uncode the token
 //Restric some methods to certain users
-const secret = 'SECRET_KEY_FOR_URAN_APP_PROJECT_170911'
+const secret = "SECRET_KEY_FOR_URAN_APP_PROJECT_170911";
 
 //Function to generate tokens, this tokens gives user info to all links
 const createToken = (user) => {
+  //iat date of session exp expire date
+  const payload = {
+    _id: user._id,
+    username: user.username,
+    iat: moment().unix(),
+    exp: moment().add(10, "days").unix(),
+  };
 
-    //iat date of session
-    //exp expire date
-    const payload = {
-        _id: user._id,
-        username : user.username,
-        iat: moment().unix(),
-        exp: moment().add(10,'days').unix()
-    }
-
-    //Return encoded token
-    return jwt.encode(payload, secret);
-}
+  //Return encoded token
+  return jwt.encode(payload, secret);
+};
 
 module.exports = {
-    secret,
-    createToken
-}
+  secret,
+  createToken,
+};
